b2141d323f42236e6fc8c66c46890c9017aeb924
FinSmy <findadamsmyth@gmail.com>
2022-11-30 10:58:02 +0000

    Addition of freertos components (to be tested)

--- src/main.cpp
+++ src/main.cpp
@@ -15,72 +15,43 @@ const int ledPin = 33;
 /*================VARIABLE DEFINITIONS================*/
 int stepnumber = 0;
 int Pa; int Pb;
-unsigned long last_time = millis(); 
-unsigned int time_now;
-
-
 
 /*================LEDC DUTY CYCLE SETUP================*/
 #define LEDC_TIMER_12_BIT 12
 #define LEDC_BASE_FREQ  5000
 
+/*================Struct Definitions================*/
+//input parameters for ledc analogWrite function
+typedef struct{
+  uint8_t channel;
+  uint32_t value; 
+  uint32_t valueMax = 255;  
+} ledc_aw_params;
+
+//input parameters for move function
+typedef struct {
+  int stepnumber; 
+  int MAXpower; 
+  int wait; 
+} move_params;
 
 /*================Function Definitions================*/
 //Analogwrite using LEDC capabilities
   void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
-    uint32_t duty = (4095/valueMax)*min(value, valueMax); 
+    //casting the input struct from void back to correct struct
+    //ledc_aw_params ledc_param = *(ledc_aw_params *) data;
 
+    //calculation of duty cycle
+    //uint32_t duty = (4095/ledc_param.valueMax)*min(ledc_param.value, ledc_param.valueMax); 
+    uint32_t duty = (4095/valueMax)*min(value, valueMax); 
     ledcWrite(channel,duty);
   }
 
-//delaying function
-void update_delay(){
-  last_time = millis();
-}
   
   
-
-
-void setup() {
- pinMode (ledPin, OUTPUT);
-
- ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_12_BIT); 
- ledcSetup(LEDC_CHANNEL_1, LEDC_BASE_FREQ, LEDC_TIMER_12_BIT);
- ledcSetup(LEDC_CHANNEL_2, LEDC_BASE_FREQ, LEDC_TIMER_12_BIT);
- ledcSetup(LEDC_CHANNEL_3, LEDC_BASE_FREQ, LEDC_TIMER_12_BIT);
-
- ledcAttachPin(A1A,LEDC_CHANNEL_0);
- ledcAttachPin(A1B,LEDC_CHANNEL_1);
- ledcAttachPin(B1A,LEDC_CHANNEL_2);
- ledcAttachPin(B2A,LEDC_CHANNEL_3);
-  Serial.begin(115200);
-
-}
-
-void loop() {
-  //digitalWrite (ledPin, HIGH);  // turn on the LED
-  //delay(500); // wait for half a second or 500 milliseconds
-  //digitalWrite (ledPin, LOW); // turn off the LED
-  //delay(500); // wait for half a second or 500 milliseconds
-  
-  for (int i=0; i<3199; i++)
-  {
-    stepnumber++;
-    move(stepnumber,255,250);
-    }
-
-    delay(3000);
-    for (int i=0; i<1599; i++)
-    { 
-      stepnumber--;
-      move(stepnumber,255,1000);
-      }
-    delay(3000);
-} 
-
 void move(int stepnumber, int MAXpower, int wait) {
-  time_now = millis();
-  
+    //move_params instruction = *(move_params *) instructions;
+
     Pa = (sin(stepnumber*0.098174)*MAXpower);
     Pb = (cos(stepnumber*0.098174)*MAXpower);
 
@@ -106,7 +77,74 @@ void move(int stepnumber, int MAXpower, int wait) {
       ledcAnalogWrite(LEDC_CHANNEL_2,0);
       ledcAnalogWrite(LEDC_CHANNEL_3,abs(Pb));
     }
+}
 
+void move_routine(void *parameters) {
 
-    
-  }
+  for (int i=0; i<3199; i++)
+  {
+    stepnumber++;
+    move(stepnumber,255,250);
+    }
+
+    delay(3000);
+    for (int i=0; i<1599; i++)
+    { 
+      stepnumber--;
+      move(stepnumber,255,1000);
+      }
+
+    vTaskDelay(3000/portTICK_PERIOD_MS);
+}
+
+void led_flash(void *parameters) {
+
+  digitalWrite (ledPin, HIGH);  // turn on the LED
+  delay(500); // wait for half a second or 500 milliseconds
+  digitalWrite (ledPin, LOW); // turn off the LED
+  delay(500); // wait for half a second or 500 milliseconds
+}
+
+void setup() {
+ pinMode (ledPin, OUTPUT);
+
+  ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_12_BIT); 
+  ledcSetup(LEDC_CHANNEL_1, LEDC_BASE_FREQ, LEDC_TIMER_12_BIT);
+  ledcSetup(LEDC_CHANNEL_2, LEDC_BASE_FREQ, LEDC_TIMER_12_BIT);
+  ledcSetup(LEDC_CHANNEL_3, LEDC_BASE_FREQ, LEDC_TIMER_12_BIT);
+
+  ledcAttachPin(A1A,LEDC_CHANNEL_0);
+  ledcAttachPin(A1B,LEDC_CHANNEL_1);
+  ledcAttachPin(B1A,LEDC_CHANNEL_2);
+  ledcAttachPin(B2A,LEDC_CHANNEL_3);
+    Serial.begin(115200);
+
+  //pinning move_routine function to core
+  xTaskCreatePinnedToCore(
+    move_routine, 
+    "move routine", 
+    2048, 
+    NULL, 
+    1, 
+    NULL, 
+    APP_CPU_NUM
+  );
+
+  //pinning led_flash function to core 
+  xTaskCreatePinnedToCore(
+    led_flash, 
+    "led flash", 
+    1024, 
+    NULL, 
+    1, 
+    NULL, 
+    APP_CPU_NUM
+  );
+
+  //Delete "setup and loop" task 
+  vTaskDelete(NULL);
+}
+
+void loop() {
+  
+} 
