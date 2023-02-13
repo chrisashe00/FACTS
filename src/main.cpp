c5c4c93c7b10839e6ca8c94d4e5edc78458e6924
chrisashe00 <chris.ashe.2018@uni.strath.ac.uk>
2023-02-13 21:02:01 +0000

    XYZ control

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,22 @@
 #include <Stepper.h>
 
 const int stepsPerRevolution = 200; // change this to match the number of steps per revolution for your motor
+const int stepsPerRevolutionx = 200;
+const int stepsPerRevolutiony = 200;
 const float stepAngle = 1.8; // NEMA 11 step angle in degrees
 const float stepRad = stepAngle * (3.141 / 180); // step angle in radians 
 
-Stepper myStepper(stepsPerRevolution, 14, 27, 26, 25); // initialize the stepper library with the number of steps per revolution and the pin numbers
+Stepper myStepperz(stepsPerRevolution, 14, 27, 26, 25); // initialize the stepper library with the number of steps per revolution and the pin numbers
+Stepper myStepperx(stepsPerRevolutionx,5,18,19,22);
+Stepper mySteppery(stepsPerRevolutiony,22,23,19,21);
 
 int ZPosition = 0;  //initial positions
 int YPosition = 0;
 int XPosition = 0; 
-float Revs = 4; //Enter number of revs here
-float linDisp = 4 * stepsPerRevolution * 0.0314 * 2.5; //Rotational to Linear Displacement
+float Revs = 1; //Enter number of revs here
+float linDispScrew = ( 3.141 * 2.84 * Revs ) / 0.5;
 
-int count1 = 0;
-
-void task1(void * parameters)
+void stepz(void * parameters)
 {
     for(;;)
     {
@@ -27,7 +29,7 @@ void task1(void * parameters)
             {
                 Serial.println("Displacing -Z");
         
-                ZPosition = ZPosition - Revs;
+                ZPosition = ZPosition - linDispScrew;
         
 
                 Serial.print("\n");
@@ -36,93 +38,185 @@ void task1(void * parameters)
                 Serial.println("mm");
         
 
-                myStepper.step(- 4 * stepsPerRevolution); // rotate the stepper motor by a quarter turn in the counterclockwise direction
+                myStepperz.step(- Revs * stepsPerRevolution); // rotate the stepper motor by a quarter turn in the counterclockwise direction
             }
 
             else if (received1 == 'u')
             {
                 Serial.println("Displacing +Z");
 
-                ZPosition = ZPosition + Revs ;
-
+                ZPosition = ZPosition + linDispScrew ;
 
                 Serial.print("\n");
                 Serial.print("Z Position:  ");
                 Serial.print(ZPosition);
                 Serial.println("mm");
 
-                myStepper.step(4 * stepsPerRevolution); // rotate the stepper motor by a quarter turn in the clockwise direction
+                myStepperz.step(Revs * stepsPerRevolution); // rotate the stepper motor by a quarter turn in the clockwise direction
+            }
+        
+
+            else if (received1 == 'r')
+            { 
+                ZPosition = 0;
+
+                Serial.println("Z Position set to 0 mm.");
             }
         }
+            
+        // Print the stack high water mark for task1
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // Serial.print("stepz stack high water mark: ");
+        // Serial.println(uxTaskGetStackHighWaterMark(NULL));
     }
 }
+
+
+void stepy(void * parameters)
+{
+    for(;;)
+    {
+        if (Serial.available())
+        {
+
+            char receivedy = Serial.read();
+            if (receivedy == 'v')
+            {
+                Serial.println("Displacing -Y");
+        
+                YPosition = YPosition - linDispScrew;
+        
+
+                Serial.print("\n");
+                Serial.print("Y position:   ");
+                Serial.print(ZPosition);
+                Serial.println("mm");
+        
+
+                mySteppery.step(- stepsPerRevolution); // rotate the stepper motor by a quarter turn in the counterclockwise direction
+            }
+
+            else if (receivedy == 'b')
+            {
+                Serial.println("Displacing +Y");
+
+                YPosition = YPosition + linDispScrew ;
+
+                Serial.print("\n");
+                Serial.print("Y Position:  ");
+                Serial.print(YPosition);
+                Serial.println("mm");
+
+                mySteppery.step(stepsPerRevolution); // rotate the stepper motor by a quarter turn in the clockwise direction
+            }
+        
+
+            else if (receivedy == 'r')
+            { 
+                YPosition = 0;
+
+                Serial.println("Y Position set to 0 mm.");
+            }
+        }
+            
+        // Print the stack high water mark for task1
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // Serial.print("stepy stack high water mark: ");
+        // Serial.println(uxTaskGetStackHighWaterMark(NULL));
+    }
+}
+
+void stepx(void * parameters)
+{
+    for(;;)
+    {
+        if (Serial.available())
+        {
+
+            char receivedx = Serial.read();
+            if (receivedx == 'c')
+            {
+                Serial.println("Displacing -X (left)");
+        
+                XPosition = XPosition - linDispScrew;
+        
+
+                Serial.print("\n");
+                Serial.print("X position:   ");
+                Serial.print(XPosition);
+                Serial.println("mm");
+        
+
+                myStepperx.step(stepsPerRevolution); // rotate the stepper motor by a quarter turn in the counterclockwise direction
+            }
+
+            else if (receivedx == 'x')
+            {
+                Serial.println("Displacing +X (right)");
+
+                XPosition = XPosition + linDispScrew ;
+
+                Serial.print("\n");
+                Serial.print("X Position:  ");
+                Serial.print(XPosition);
+                Serial.println("mm");
+
+                myStepperx.step(stepsPerRevolution); // rotate the stepper motor by a quarter turn in the clockwise direction
+            }
+        
+
+            else if (receivedx == 'r')
+            { 
+                YPosition = 0;
+
+                Serial.println("X Position set to 0 mm.");
+            }
+        }
+            
+        // Print the stack high water mark for task1
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // Serial.print("stepx stack high water mark: ");
+        // Serial.println(uxTaskGetStackHighWaterMark(NULL));
+    }
+}
+
 
 void setup()
 {
     Serial.begin(115200); // initialize serial communication
-    myStepper.setSpeed(60); // set the speed of the stepper motor
+    myStepperz.setSpeed(60); // set the speed of the stepper motor
+    myStepperx.setSpeed(60);
+    mySteppery.setSpeed(60);
+
 
     xTaskCreate(
-        task1,
-        "task1",
-        1000,
+        stepz,
+        "stepz",
+        5000,
+        NULL,
+        NULL
+    );
+
+        xTaskCreate(
+        stepy,
+        "stepy",
+        5000,
         NULL,
         1,
         NULL
     );
-}
 
-void loop()
-{
-    if (Serial.available())
-    {
-        char received1 = Serial.read();
-        if (received1 == 'd')
-        {
-            Serial.println("Displacing -Z");
-            
-            ZPosition = ZPosition - Revs;
-            
-
-            Serial.print("\n");
-            Serial.print("Z position:   ");
-            Serial.print(ZPosition);
-            Serial.println("mm");
-            
-
-            myStepper.step(- 4 * stepsPerRevolution); // rotate the stepper motor by a quarter turn in the counterclockwise direction
-        }
-        else if (received1 == 'u')
-        {
-            Serial.println("Displacing +Z");
-
-            ZPosition = ZPosition + Revs ;
-
-
-            Serial.print("\n");
-            Serial.print("Z Position:  ");
-            Serial.print(ZPosition);
-            Serial.println("mm");
-
-            myStepper.step(4 * stepsPerRevolution); // rotate the stepper motor by a quarter turn in the clockwise direction
-        }
-
-    }
-
-    if (Serial.available() > 0) {
-        String input = Serial.readString();
-        if (input.equals("r")) {
-
-            ZPosition = 0;
-
-            Serial.println("Z Position set to 0 mm.");
-        }
-    }
-
+        xTaskCreate(
+        stepx,
+        "stepx",
+        5000,
+        NULL,
+        1,
+        NULL
+    );
 
 
 }
 
 
-
-
+void loop(){}
