c7b4286fcee92df74b2c7e807111330031adae8b
chrisashe00 <chris.ashe.2018@uni.strath.ac.uk>
2023-02-15 13:41:31 +0000

    unsure whats wrong

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,14 @@ const int stepsPerRevolutiony = 200;
 const float stepAngle = 1.8; // NEMA 11 step angle in degrees
 const float stepRad = stepAngle * (3.141 / 180); // step angle in radians 
 
-Stepper myStepperz(stepsPerRevolution, 14, 27, 26, 25); // initialize the stepper library with the number of steps per revolution and the pin numbers
-Stepper myStepperx(stepsPerRevolutionx,5,18,19,22);
-Stepper mySteppery(stepsPerRevolutiony,22,23,19,21);
+Stepper myStepperz(stepsPerRevolution, 1,2,3,4); // initialize the stepper library with the number of steps per revolution and the pin numbers
+Stepper myStepperx(stepsPerRevolutionx,14,27,26,25);
+Stepper mySteppery(stepsPerRevolutiony,33,32,35,34);
 
 int ZPosition = 0;  //initial positions
 int YPosition = 0;
 int XPosition = 0; 
-float Revs = 1; //Enter number of revs here
+float Revs = 0.5; //Enter number of revs here
 float linDispScrew = ( 3.141 * 2.84 * Revs ) / 0.5;
 
 void stepz(void * parameters)
@@ -121,8 +121,8 @@ void stepy(void * parameters)
             
         // Print the stack high water mark for task1
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        // Serial.print("stepy stack high water mark: ");
-        // Serial.println(uxTaskGetStackHighWaterMark(NULL));
+        Serial.print("stepy stack high water mark: ");
+        Serial.println(uxTaskGetStackHighWaterMark(NULL));
     }
 }
 
@@ -175,46 +175,56 @@ void stepx(void * parameters)
             
         // Print the stack high water mark for task1
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        // Serial.print("stepx stack high water mark: ");
-        // Serial.println(uxTaskGetStackHighWaterMark(NULL));
+        Serial.print("stepx stack high water mark: ");
+        Serial.println(uxTaskGetStackHighWaterMark(NULL));
     }
 }
 
 
 void setup()
 {
+        // Set the pin mode for the pins to be output
+    pinMode(14, OUTPUT);
+    pinMode(27, OUTPUT);
+    pinMode(26, OUTPUT);
+    pinMode(25, OUTPUT);
+    pinMode(33, OUTPUT);
+    pinMode(32, OUTPUT);
+    pinMode(35, OUTPUT);
+    pinMode(34, OUTPUT);
+
     Serial.begin(115200); // initialize serial communication
     myStepperz.setSpeed(60); // set the speed of the stepper motor
     myStepperx.setSpeed(60);
     mySteppery.setSpeed(60);
 
 
-    xTaskCreate(
-        stepz,
-        "stepz",
-        5000,
-        NULL,
-        NULL
-    );
+    // xTaskCreate(
+    //     stepz,
+    //     "stepz",
+    //     5000,
+    //     NULL,
+    //     1,
+    //     NULL
+    // );
 
         xTaskCreate(
         stepy,
         "stepy",
-        5000,
+        6000,
         NULL,
-        1,
+        2,
         NULL
     );
 
         xTaskCreate(
         stepx,
         "stepx",
-        5000,
+        6000,
         NULL,
         1,
         NULL
     );
-
 
 }
 
