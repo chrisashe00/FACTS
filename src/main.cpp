ccd5698d9b42dde0f9541237d66783f6dba16790
chrisashe00 <chris.ashe.2018@uni.strath.ac.uk>
2023-02-16 12:50:22 +0000

    Only driving one motor from this

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,27 @@
 #include <Arduino.h>
 #include <Stepper.h>
 
-const int step_360 = 200; // 360 number of steps per/rev
-// initialize the stepper library on pins 2-5 n 8-11
-Stepper myStepper1(step_360,14,27,26,25);
-Stepper myStepper2(step_360,5,18,19,21);
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+// for your motor
 
-void setup()
-{
-// set the speed at 60 rpm:
-myStepper1.setSpeed(60);//left
-myStepper2.setSpeed(60);//right
-// initialize the serial port:
-Serial.begin(115200);
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 14, 27, 26, 25);
+
+void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(30);
+  // initialize the serial port:
+  Serial.begin(115200);
 }
-void loop()
-{
-// step one revolution in one direction:
-Serial.println("clockwise");
-myStepper1.step(step_360);
-delay(500);
+
+void loop() {
+  // step one revolution  in one direction:
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
 }
