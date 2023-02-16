04084e91cf3daf622871b1df8454ee74b97d2100
chrisashe00 <chris.ashe.2018@uni.strath.ac.uk>
2023-02-16 15:47:18 +0000

    Some Test sketches

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,45 @@
 #include <Arduino.h>
+#include <Stepper.h>
 
-const byte led_gpio = 32;
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+// for your motor
 
-// the setup function runs once when you press reset or power the board
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 14, 27, 26, 25);
+// define LED pin
+const int LED_PIN = 12; // change this to match the pin you've connected the LED to
+int brightness = 128; // starting brightness for the LED
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(led_gpio, OUTPUT);
+  // initialize serial communication
+  Serial.begin(115200);
+  // set LED pin as output
+  pinMode(LED_PIN, OUTPUT);
+
+  myStepper.setSpeed(60);
+  // initialize the serial port:
+
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(led_gpio, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(led_gpio, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  // read serial input
+  // if (Serial.available()) {
+  //   char input = Serial.read();
+  //   if (input == '8') {
+  //     brightness = max(0, brightness - 32); // decrease brightness by 32 (out of 255)
+  //     analogWrite(LED_PIN, brightness);
+  //   } else if (input == '9') {
+  //     brightness = min(255, brightness + 32); // increase brightness by 32 (out of 255)
+  //     analogWrite(LED_PIN, brightness);
+  //   }
+  // }
+
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
 }
