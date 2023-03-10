c586eecdb7fce3614904f7d8cb6c0a26b8c762e2
chrisashe00 <chris.ashe.2018@uni.strath.ac.uk>
2023-03-10 16:20:21 +0000

    loggin out

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Stepper.h>
+#include <Ps3Controller.h>
 
 // change this to the number of steps on your motor
 #define STEPS 200
@@ -12,6 +13,9 @@ Stepper stepper(STEPS, 14, 27 ,26, 25);
 
 void setup()
 {
+  Ps3.begin("01:02:03:04:05:06");
+  Serial.println("Ready.");
+
   Serial.begin(115200);
   Serial.println("Stepper test!");
   // set the speed of the motor to 30 RPMs
@@ -20,8 +24,15 @@ void setup()
 
 void loop()
 {
-  Serial.println("Forward");
-  stepper.step(STEPS);
-  Serial.println("Backward");
-  stepper.step(-STEPS);
+  if (Ps3.isConnected()){
+    Serial.println("Connected!");
+  }
+  delay(3000);
+
+  if (Serial.available > 0 ){
+    const char command == Serial.read();
+
+    if (command == 'h'){}
+    
+  }
 }
