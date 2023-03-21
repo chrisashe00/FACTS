ed7d61542b71dadd1d9767c8d7fe30adde2a7209
chrisashe00 <chris.ashe.2018@uni.strath.ac.uk>
2023-03-21 14:49:41 +0000

    retreived code and rewrote

--- src/main.cpp
+++ src/main.cpp
@@ -14,34 +14,31 @@ const int XA1B = 18;
 const int XB1A = 19;
 const int XB1B = 21;
 
-const int YA1A = 5; //Y Stage Stepper Pins
+const int YA1A = 5; //X Stage Stepper Pins
 const int YA1B = 18;
 const int YB1A = 19;
 const int YB1B = 21;
 
-const int ZA1A = 14; //Z Stage Stepper Pins
-const int ZA1B = 27;
-const int ZB1A = 26;
-const int ZB1B = 25;
-const int ZSLP = 33;
-const int ZULT = 32;
+const int ZA1A = 5; //Z Stage Stepper Pins
+const int ZA1B = 18;
+const int ZB1A = 19;
+const int ZB1B = 21;
 
-const int fanCMDPin = 13; // Fan PWM pin 
-const int limeLedPin = 0; // Lime LED pin
-const int blueLedPin = 0; //Blue LED pin
+const int blueLedPin = 12; // Blue LED
+const int limeLedPin = 13; // Lime LED
 
-// ---------------- PS3 Controller Initialise ------------ //
-int player = 0;
-int battery = 0;
+const int xControlPin = 15; //X,Y,Z Switches
+const int yControlPin = 16;
+const int zControlPin = 17;
 
 // ---------------- Variables ------------ //
 
-const int stepsPerRev = 200; // motor steps for one revolution
+const int stepsPerRevolution = 200; // motor steps for one revolution
 const float stepAngle = 1.8; // motor step angle in degrees
+const float stepRad = stepAngle * (3.141 / 180); // step angle in radians 
 
-int blueBrightness = 255; // starting brightness for the blue LED
-int limeBrightness = 255; // starting brightness for the lime LED
-int fanBrightness = 255;
+int blueBrightness = 0; // starting brightness for the blue LED
+int limeBrightness = 0; // starting brightness for the lime LED
 
 int fadeAmount = 17; // How much to change the brightness each step when fading
 bool isFading = false; // Whether the LED is currently fading
@@ -50,98 +47,80 @@ int xPosition = 0;  //initialising stepper positions
 int yPosition = 0;
 int zPosition = 0;
 
+float Revs = 1; //Enter number of Revs here
+
+// Screw Equation, gives linear displacement of Z stage
+float linDispScrew = tan(0.5236) * 0.5 * 2 * 3.14159; 
 
 // ---------------- Stepper.h functions ------------ //
 
-Stepper myStepperX(stepsPerRev,XA1A,XA1B,XB1A,XB1B); // X
-Stepper myStepperY(stepsPerRev,YA1A,YA1B,YB1A,YB1B); // Y 
-Stepper myStepperZ(stepsPerRev,ZA1A,ZA1B,ZB1A,ZB1B); // Z 
-
-// ------------------ Connect PS3 controller -------------- //
-// void onConnect(){
-//     Serial.println("Connected.");
-// }
+Stepper myStepperX(stepsPerRevolution,XA1A,XA1B,XB1A,XB1B); // X
+Stepper myStepperY(stepsPerRevolution,YA1A,YA1B,YB1A,YB1B); // Y 
+Stepper myStepperZ(stepsPerRevolution,ZA1A,ZA1B,ZB1A,ZB1B); // Z 
 
 // ------------------ Z Stage Stepper function -------------- //
 
 void stepZ(void *parameters) {
     for (;;) {
-        if (Serial.available()){
+        if (Serial.available()) {
             char receivedZ = Serial.read();
-            switch(receivedZ){
-                case '1':
-                    digitalWrite(ZSLP,HIGH);
-                    vTaskDelay(50 / portTICK_PERIOD_MS);
-                    myStepperZ.step(1);
-                    Serial.println("Z stage performing +1 step");
-                    digitalWrite(ZSLP, LOW);
-                    break;
-
-                case '2':
-                    digitalWrite(ZSLP,HIGH);
-                    vTaskDelay(50 / portTICK_PERIOD_MS);
-                    myStepperZ.step(-1);
-                    Serial.println("Z stage performing -1 step");
-                    digitalWrite(ZSLP, LOW);
-                    break;
-
-                case '3':
-                    digitalWrite(ZSLP,HIGH);
-                    myStepperZ.step(10);
-                    Serial.println("Z stage performing +10 steps");
-                    digitalWrite(ZSLP, LOW);
-                    break;
-
-                case '4':
-                    digitalWrite(ZSLP,HIGH);
-                    myStepperZ.step(-10);
-                    Serial.println("Z stage performing -10 steps");
-                    digitalWrite(ZSLP, LOW);
-                    break;
-
-                case '5':
-                    digitalWrite(ZSLP,HIGH);
-                    myStepperZ.step(50);
-                    Serial.println("Z stage performing 50 steps");
-                    digitalWrite(ZSLP, LOW);
-                    break;
-
-                case '6':
-                    digitalWrite(ZSLP,HIGH);
-                    myStepperZ.step(-50);
-                    Serial.println("Z stage performing -50 steps");
-                    digitalWrite(ZSLP, LOW);
-                    break;
-
-                case '7':
-                    digitalWrite(ZSLP,HIGH);
-                    myStepperZ.step(100);
-                    Serial.println("Z stage performing 100 steps");
-                    digitalWrite(ZSLP, LOW);
-                    break;
-                case '8':
-                    digitalWrite(ZSLP,HIGH);
-                    myStepperZ.step(-100);
-                    Serial.println("Z stage performing -100 steps");
-                    digitalWrite(ZSLP, LOW);
-                    break;
-                    
-                default:
-                    vTaskDelay(50 / portTICK_PERIOD_MS); //small delay between brightness updates
-                    break;
+            if (receivedZ == 'd') {
+                Serial.println("Displacing -Z");
+                zPosition = zPosition - linDispScrew; //What ESP32 thinks displacement is
+                Serial.println("\nZ position: ");
+                Serial.println(zPosition);
+                Serial.println("mm");
+                //This performs actual rotation, should translate to Downward displacement
+                myStepperZ.step(-Revs * stepsPerRevolution); 
             }
-        
+            else if (receivedZ == 'u') {
+                Serial.println("Displacing +Z");
+                zPosition = zPosition + linDispScrew ; //What ESP32 thinks displacement is
+                Serial.print("\nZ Position:  ");
+                Serial.print(zPosition);
+                Serial.println("mm");
+                //This performs actual rotation, should translate to Upward displacement
+                myStepperZ.step(Revs * stepsPerRevolution);
+            }
+            else if (receivedZ == 'r') {
+                zPosition = 0; //Reset the gauge displacement if char received is an r
+                Serial.println("Z Position set to 0 mm.");
+            }
         }
-    vTaskDelay(50 / portTICK_PERIOD_MS); //small delay between brightness updates
+        vTaskDelay(30 / portTICK_PERIOD_MS); //This adds a short delay before starting a queued function
     }
 }
-
 
 // ------------------ Y Stage Stepper function -------------- //
 
 void stepY(void *parameters) {
     for (;;) {
- 
+        if (Serial.available()) {
+            char receivedY = Serial.read();
+            if (receivedY == 'l') {
+                Serial.println("Displacing -Y");
+                yPosition = yPosition - linDispScrew; //What ESP32 thinks displacement is
+                Serial.println("\nY position: ");
+                Serial.println(yPosition);
+                Serial.println("mm");
+                //This performs actual rotation, should translate to -Y displacement
+                myStepperY.step(-Revs * stepsPerRevolution); 
+            }
+            else if (receivedY == 'r') {
+                Serial.println("Displacing +Y");
+                yPosition = yPosition + linDispScrew ; //What ESP32 thinks displacement is
+                Serial.print("\nY Position: ");
+                Serial.print(yPosition);
+                Serial.println("mm");
+                //This performs actual rotation, should translate to Upward displacement
+                myStepperY.step(Revs * stepsPerRevolution);
+            }
+            else if (receivedY == 'r') {
+                zPosition = 0; //Reset the gauge displacement if char received is an r
+                Serial.println("Y Position set to 0 mm.");
+            }
+        }
+        vTaskDelay(30 / portTICK_PERIOD_MS); //This adds a short delay before starting a queued function
     }
 }
 
@@ -149,7 +128,32 @@ void stepY(void *parameters) {
 
 void stepX(void *parameters) {
     for (;;) {
-  
+        if (Serial.available()) {
+            char receivedX = Serial.read();
+            if (receivedX == 'l') {
+                Serial.println("Displacing -X");
+                xPosition = xPosition - linDispScrew; //What ESP32 thinks displacement is
+                Serial.println("\nx position: ");
+                Serial.println(xPosition);
+                Serial.println("mm");
+                //This performs actual rotation, should translate to -Y displacement
+                myStepperX.step(-Revs * stepsPerRevolution); 
+            }
+            else if (receivedX == 'r') {
+                Serial.println("Displacing +X");
+                xPosition = xPosition + linDispScrew ; //What ESP32 thinks displacement is
+                Serial.print("\nX Position: ");
+                Serial.print(xPosition);
+                Serial.println("mm");
+                //This performs actual rotation, should translate to Upward displacement
+                myStepperX.step(Revs * stepsPerRevolution);
+            }
+            else if (receivedX == 'r') {
+                xPosition = 0; //Reset the gauge displacement if char received is an r
+                Serial.println("X Position set to 0 mm.");
+            }
+        }
+        vTaskDelay(30 / portTICK_PERIOD_MS); //This adds a short delay before starting a queued function
     }
 }
 
@@ -157,7 +161,47 @@ void stepX(void *parameters) {
 
 void blueLed(void *parameters) {
     for (;;) {
-       
+        if (Serial.available()) {
+            char blueInput = Serial.read();
+            if (blueInput == '1'){
+                blueBrightness = max(0, blueBrightness - 32); // decrease brightness by 32 (out of 255)
+                analogWrite(blueLedPin, blueBrightness);
+                Serial.print("\n");
+                Serial.print("Current Blue LED Brightness: ");
+                Serial.print(blueBrightness);
+                Serial.print('\n');
+            } 
+            else if (blueInput == '2'){
+                blueBrightness = min(255, blueBrightness + 32); // increase brightness by 32 (out of 255)
+                analogWrite(blueLedPin, blueBrightness);
+                Serial.print("\n");
+                Serial.print("Current Blue LED Brightness: ");
+                Serial.print(blueBrightness);
+                Serial.print('\n');
+            }
+            else if (blueInput == 'f'){
+                // Toggle fading on or off
+                isFading = !isFading;
+                if (isFading) {
+                    Serial.println("Fading started");
+                } 
+                else {
+                    Serial.println("Fading stopped");
+                }
+            }
+        }
+        if (isFading) {
+            blueBrightness += fadeAmount;
+            if (blueBrightness <= 0 || blueBrightness >= 255) {
+                // Reverse the fade direction when the brightness reaches the min or max value
+                fadeAmount = -fadeAmount;
+            }
+            analogWrite(blueLedPin, blueBrightness); //Write the updated brightness value to Pin
+            vTaskDelay(50 / portTICK_PERIOD_MS); //small delay between brightness updates
+        } 
+        else {
+            blueBrightness = 255; //Otherwise have 0 Brightness
+        }
     }
 }
 
@@ -165,50 +209,59 @@ void blueLed(void *parameters) {
 
 void limeLed(void *parameters) {
     for (;;) {
-        
+        if (Serial.available()) {
+            char limeInput = Serial.read();
+            if (limeInput == '1'){
+                limeBrightness = max(0, limeBrightness - 32); // decrease brightness by 32 (out of 255)
+                analogWrite(limeLedPin, limeBrightness);
+                Serial.print("\n");
+                Serial.print("Current Lime LED Brightness: ");
+                Serial.print(limeBrightness);
+                Serial.print('\n');
+            } 
+            else if (limeInput == '2'){
+                blueBrightness = min(255, limeBrightness + 32); // increase brightness by 32 (out of 255)
+                analogWrite(limeLedPin, limeBrightness);
+                Serial.print("\n");
+                Serial.print("Current Lime LED Brightness: ");
+                Serial.print(limeBrightness);
+                Serial.print('\n');
+            }
+            else if (limeInput == 'f'){
+                // Toggle fading on or off
+                isFading = !isFading;
+                if (isFading) {
+                    Serial.println("Fading started");
+                } 
+                else {
+                    Serial.println("Fading stopped");
+                }
+            }
+        }
+        if (isFading) {
+            limeBrightness += fadeAmount;
+            if (limeBrightness <= 0 || limeBrightness >= 255) {
+                // Reverse the fade direction when the brightness reaches the min or max value
+                fadeAmount = -fadeAmount;
+            }
+            analogWrite(limeLedPin, blueBrightness); //Write the updated brightness value to Pin
+            vTaskDelay(50 / portTICK_PERIOD_MS); //small delay between brightness updates
+        } 
+        else {
+            limeBrightness = 255; //Otherwise have 0 Brightness
+        }
     }
 }
-
-void fanPWM(void *parameters) {
-    for (;;) {
-        
-    }
-}
-
-
-
 
 // ---------------- Setup: Simply calls the above Functions ------------ //
 
 void setup()
 {
-    // Ps3.attachOnConnect(onConnect);
-    // Ps3.begin("C0:49:EF:D3:6C:D6");
-
-    // Serial.println("Ready.");
-    // Serial.print("Setting LEDs to player "); 
-    // Serial.println(player, DEC);
-    // Ps3.setPlayer(player);
-
-    // player = 1;
-
-    // pinMode(limeLedPin, OUTPUT);
-    // pinMode(blueLedPin,OUTPUT);
-    pinMode(fanCMDPin, OUTPUT);
-
-    pinMode(ZA1A, OUTPUT);
-    pinMode(ZA1A, OUTPUT);
-    pinMode(ZA1A, OUTPUT);
-    pinMode(ZA1A, OUTPUT);
-
-    pinMode(ZSLP, OUTPUT);
-    pinMode(ZULT, OUTPUT);
-
-    
-    // default SLP and ULT pins to low 
-    digitalWrite(ZSLP,LOW);
-    digitalWrite(ZULT, LOW);
-
+    pinMode(limeLedPin, OUTPUT);
+    pinMode(blueLedPin,OUTPUT);
+    pinMode(xControlPin,OUTPUT);
+    pinMode(yControlPin,OUTPUT);
+    pinMode(zControlPin,OUTPUT);
     Serial.begin(115200); 
     myStepperZ.setSpeed(60); // set the speed of the stepper motors to 60 rpm
     myStepperX.setSpeed(60);
@@ -216,24 +269,14 @@ void setup()
 
 // ---------------- Function Calls ------------ //
 
-    xTaskCreate(
-        stepZ,
-        "stepZ",
-        2048,
-        NULL,
-        1,
-        NULL
-    );
-
-    xTaskCreate(
-        fanPWM,
-        "fanPWM",
-        2048,
-        NULL,
-        0,
-        NULL
-    );
-
+    // xTaskCreate(
+    //     stepZ,
+    //     "stepZ",
+    //     1000,
+    //     NULL,
+    //     1,
+    //     0
+    // );
 
     //     xTaskCreate(
     //     blueLed,
@@ -247,10 +290,10 @@ void setup()
     //     xTaskCreate(
     //     limeLed,
     //     "limeLed",
-    //     8000,
+    //     1000,
     //     NULL,
     //     1,
-    //     NULL
+    //     1
     // );
 
     //     xTaskCreate(
@@ -262,19 +305,18 @@ void setup()
     //     0
     // );
 
-    //     xTaskCreate(
-    //     stepX,
-    //     "stepX",
-    //     1000,
-    //     NULL,
-    //     1,
-    //     0
-    // );
+        xTaskCreate(
+        stepX,
+        "stepX",
+        1000,
+        NULL,
+        1,
+        0
+    );
 
 }
 
 
 // Loop Not used //
 void loop(){
-
 }
